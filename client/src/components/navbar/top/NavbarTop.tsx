578fd3bb9ba0fd6419f372eb1d1dd1e53c75e9c0
Florent Paris <florentparis28@gmail.com>
2022-05-01 15:13:14 +0200

    preparation project done

---
+++ client/src/components/navbar/top/NavbarTop.tsx
@@ -0,0 +1,5 @@
+export default function NavbarTop() {
+    return (
+        <div>NavbarTop</div>
+    )
+}
---
+++ client/src/components/navbar/vertical/NavbarVertical.tsx
@@ -0,0 +1,5 @@
+export default function NavbarVertical() {
+    return (
+        <div>NavbarVertical</div>
+    )
+}
