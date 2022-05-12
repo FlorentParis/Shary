76d5fa275b3b1d0ad4ccf53d8e8ffc6b084c8c95
Maxime Palmino <maximepalmino.pro@gmail.com>
2022-05-12 15:40:19 +0200

    [ADD]Login-Register-Components

---
+++ client/src/components/authentification/Register.tsx
@@ -0,0 +1,12 @@
+
+
+const Register = () => {
+
+    return (
+        <>
+            <h1>Hello from Register</h1>
+        </>
+    )
+}
+
+export default Register
---
+++ client/src/pages/authentification/Auth.tsx
@@ -0,0 +1,13 @@
+import styles from '../../style/pages/authentification/Auth.module.scss'
+
+
+const Auth: React.FC = () => {
+
+    return (
+        <>
+        </>
+
+    )
+}
+
+export default Auth
