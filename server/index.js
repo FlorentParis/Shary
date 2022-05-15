593ab14f96815aed46ecf28039bacf67c4073752
GiselleMM <giselle.magalong@gmail.com>
2022-05-15 10:49:30 +0200

    [ADD]UserFiles

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to Shary server" });
 });
 // set port, listen for requests
 const PORT = process.env.PORT || 3030;
