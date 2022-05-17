2c90dafeeb9bd4f2b586ca8a4ea9c757e8970b76
Florent <florentparis28@gmail.com>
2022-05-17 15:30:08 +0200

    without prob

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style/App.scss';
-import store from './stores/store'
+/* import store from './store/index' */
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
+  /* <Provider store={store}> */
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  /* </Provider> */
 );
