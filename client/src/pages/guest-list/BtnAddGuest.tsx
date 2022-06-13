bd0c766877f46249df9f7caa3e333d060b6c4151
Florent Paris <florentparis28@gmail.com>
2022-06-13 15:18:39 +0200

    page guest-list

---
+++ client/src/pages/guest-list/BtnAddGuest.tsx
@@ -0,0 +1,10 @@
+export default function BtnAddGuest() {
+    return (
+        <div className="guest-card btn-add-guest">
+            <div className="container-img">
+                <span>+</span>
+            </div>
+            <span>Ajouter de nouveaux participants</span>
+        </div>
+    )
+}
---
+++ client/src/pages/guest-list/GuestCard.tsx
@@ -0,0 +1,15 @@
+export default function GuestCard() {
+    return (
+        <div className="guest-card">
+            <div className="container-img">
+                <img src="./img/guest-demo.png" />
+            </div>
+            <span>Maude D. McKinney</span>
+            <div>
+                <div>
+                    <span></span>Participe
+                </div>
+            </div>
+        </div>
+    )
+}
