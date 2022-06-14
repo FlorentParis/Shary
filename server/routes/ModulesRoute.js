7df02fa858b9cb7dcca0dad18377e22ae2740bf6
Symchowicz <symchowiczbenji@gmail.com>
2022-06-14 11:49:17 +0200

    [CREATE] updateModules

--- server/routes/ModulesRoute.js
+++ server/routes/ModulesRoute.js
@@ -3,11 +3,14 @@ const router = express.Router()
 
 const {
     createModules,
-    deleteModules
+    deleteModules,
+    updateModules
+
 } = require('../controllers/ModulesController.js')
 
 router.post('/createModules', createModules)
 router.post('/deleteModules', deleteModules)
+router.post('/updateModules', updateModules)
 
 
 module.exports = router
