49106548e78b5f6bd1211b76a4f30bbec9649841
Florent Paris <florentparis28@gmail.com>
2022-06-18 13:53:36 +0200

    beginning page alert

--- client/src/pages/alert/index.tsx
+++ client/src/pages/alert/index.tsx
@@ -6,7 +6,32 @@ export default function Alert() {
         <>
             <PageBanner imgSrc="./icons/announce-gradient.svg" title="Ecrire une annonce" desc="Annonces ratachées à l'évènement" />
             <PageContainer>
-                Annonce
+                <div className="page-alert">
+                    <div>
+                        <div>
+                            <img src="./prov/pp.png" />
+                            <input type="text" placeholder="Titre" />
+                        </div>
+                        <div>
+                            <select>
+                                <option value="">Paragraphe</option>
+                            </select>
+                            <div className="modif-style-text">
+                                <button style={{fontWeight: '700'}}>B</button>
+                                <button style={{fontStyle: 'italic'}}>I</button>
+                                <button style={{textDecorationLine: 'underline'}}>U</button>
+                                <button style={{textDecorationLine: 'line-through'}}>S</button>
+                            </div>
+                        </div>
+                    </div>
+                    <form action="">
+                        <textarea name="" placeholder="Message..."></textarea>
+                        <div>
+                            <span>Cancel</span>
+                            <button className="btn-send"><img src="./icons/send-plane.svg" /></button>
+                        </div>
+                    </form>
+                </div>
             </PageContainer>
         </>
     )
