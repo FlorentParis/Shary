d9345dd1387ba7d7ef5a6edafa29d36045656c45
ilyesb36 <ilyesb36130@gmail.com>
2022-07-04 11:42:30 +0200

    fix login

--- client/src/hooks/useLogin.tsx
+++ client/src/hooks/useLogin.tsx
@@ -4,12 +4,14 @@ export default function useLogin() {
     return (username: string, password: string) => {
         return axios({
             url: "http://localhost:3030/api/user/getUserConnexion",
-            method:"get",
+            method:"post",
             withCredentials: true,
             data: {
                 username: username,
                 password: password
-            }
+            },
+            headers: { 'Content-Type': 'application/json' },
+
         })
         .then(res=>res.data)
     }
