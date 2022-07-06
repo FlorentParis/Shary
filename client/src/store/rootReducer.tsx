745e35be85cb4e1d5d6d27b609f3b5fcc0f4dab7
dbnsf <d_bruneau2@hetic.eu>
2022-07-06 14:24:26 +0200

    [EVENTS-HOMEPAGE] implementation of event display conditions

--- client/src/store/rootReducer.tsx
+++ client/src/store/rootReducer.tsx
@@ -3,10 +3,14 @@ import eventsSlice from "../features/eventsSlice";
 import modulesSlice from "../features/modulesSlice";
 import userConnectedSlice from "../features/userConnectedSlice";
 import usersSlice from "../features/usersSlice";
+import currentEventSlice from '../features/currentEventSlice';
+
 
 export const rootReducer = combineReducers({
     events: eventsSlice,
     modules: modulesSlice,
     users: usersSlice,
-    userConnected: userConnectedSlice
+    userConnected: userConnectedSlice,
+    targetEvent: currentEventSlice 
+
 })
