82a18275d11b73ec9fd866033c2512461f3a532b
Symchowicz <78409966+Symchowicz@users.noreply.github.com>
2022-07-06 15:32:04 +0200

    [UPDATE] module photo

--- client/src/pages/photo/index.tsx
+++ client/src/pages/photo/index.tsx
@@ -1,13 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PageBanner from "../../components/common/PageBanner";
 import PageContainer from "../../components/common/PageContainer";
 import AsidePhoto from "./AsidePhoto";
+import useUploadCloudinary from "../../hooks/useUploadCloudinary";
+import { useAppSelector } from "../../hooks/reduxHooks";
+import useUploadModule from "../../hooks/useUploadModule";
+
+
 
 export default function Photo() {
+
+    const { io } = require("socket.io-client");
+    const socket = io.connect("http://localhost:3031");
+    socket.emit("joinRoomEvent", "chat" + "629fad3334582a973da2f7cf");
+    let [file, setFile] = useState<any>({});
+    const formData = new FormData();
+    formData.append("upload_preset", "modules");
+    formData.append("cloud_name", "dr7db2zsv");
+
 
     const [displayUpload, setDisplayUpload] = useState<boolean>(true);
     /* L'idée c'est de faire passer l'object dans le displayAside et le récupérer dans l'élément TSX */
     const [displayAside, setDisplayAside] = useState<any>(false);
+
+    const author = useAppSelector((state) => state.userConnected.id)
+    const id_event = "629fad3334582a973da2f7cf"
+
+    const uploadCloud = useUploadCloudinary();
+    const uploadModule = useUploadModule(); 
+
+    const inputFileChange = (e: any) => {
+        setFile(e.target.files![0])
+    };
+
+    useEffect(() => {
+
+        if(file.name) {
+            formData.append("file", file);
+
+            uploadCloud("image", formData)
+            .then(res=> {
+                const fileURL = res.url
+                const imageSocket = {event:id_event,file:fileURL}
+                socket.emit("upload_file", imageSocket);
+                uploadModule(fileURL, id_event, author, "photos_videos")
+                .then(res=>res.data);
+            })
+        }else {
+            console.log()
+        }
+        
+    }, [file])
+
+    useEffect(() => {
+        socket.on("receive_file", (data:any) => {
+          console.log(data.file)
+        });
+      }, [socket]);
 
     return (
         <>
@@ -32,7 +81,10 @@ export default function Photo() {
                     </div>
                     {displayUpload ? 
                         <div className="upload">
-                            <button>Glissez et déposez votre photo ou vidéo ici</button>
+                            <form >
+                                <input type="file" onChange={inputFileChange}/>
+                                <label>Envoyez votre photo ou vidéo ici</label>
+                            </form>
                         </div> 
                     : ""}
                     {displayUpload ? "" :
